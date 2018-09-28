6c492546e528a618269a1fbc4dd0c97e5d55e77b
devwiltbank <devin.wiltbank@gmail.com>
2018-09-28 09:17:43 -0700

    updated bootstrap link in html.index.

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,29 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+      this.state = {
+        name: '',
+        x: '',
+        y: '',
+      }
+     //bindings here 
+
+    
+  } 
+  
+  
+  
+  
   render() {
     return (
+
+      <div className='container'>
       <div className='App'></div>
+
+
+      </div>
     );
   }
 }
